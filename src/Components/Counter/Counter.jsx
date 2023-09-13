991f0a310e1ab1e7a541090798020d53d59e6b85
abcmehedi5 <abcmehedi5@gmail.com>
2023-09-13 22:39:15 +0600

    tailwind css added

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -9,23 +9,42 @@ import {
 } from "../../Redux/Features/CounterSlice";
 
 const Counter = () => {
-  const count = useSelector((state) => state.counter.value); // Select the value from the counter slice
+  let count = useSelector((state) => state.counter.value); // Select the value from the counter slice
   const dispatch = useDispatch();
-  const handleReset = () => {
-    count = " ";
-  };
+
   return (
-    <div>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <h1 id="count">Count: {count}</h1>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(5))}>
+    <div className="flex gap-10 flex-col ">
+      <button
+        className="bg-orange-500 p-3 rounded text-2xl "
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <h1 className="bg-gray-400 text-2xl text-white p-10 ">Count: {count}</h1>
+      <button
+        className="bg-orange-500 p-3 rounded text-2xl "
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      <button
+        className="bg-orange-500 p-3 rounded text-2xl "
+        onClick={() => dispatch(incrementByAmount(5))}
+      >
         Increment 5
       </button>
-      <button onClick={() => dispatch(decrementByAmount(5))}>
+      <button
+        className="bg-orange-500 p-3 rounded text-2xl "
+        onClick={() => dispatch(decrementByAmount(5))}
+      >
         Decrement by 5
       </button>
-      <button onClick={() => dispatch(reset())}>reset</button>
+      <button
+        className="bg-orange-500 p-3 rounded text-2xl "
+        onClick={() => dispatch(reset())}
+      >
+        reset
+      </button>
     </div>
   );
 };
