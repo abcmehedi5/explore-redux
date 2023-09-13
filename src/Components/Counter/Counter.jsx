0aff87b04389d892601e63208f6bff7afabbfae0
abcmehedi5 <abcmehedi5@gmail.com>
2023-09-13 12:24:04 +0600

    payload

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../../Redux/CounterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../../Redux/Features/CounterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value); // Select the value from the counter slice
@@ -11,6 +15,9 @@ const Counter = () => {
       <button onClick={() => dispatch(increment())}>Increment</button>
       <h1>Count: {count}</h1>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment 5
+      </button>
     </div>
   );
 };
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./CounterSlice";
+import counterReducer from "./Features/CounterSlice";
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
